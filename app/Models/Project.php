6bada987b6fd2de3fe779234046f5d77abcb36ba
Anatoliegit88 <anatolie.coroban88@gmail.com>
2023-01-19 17:41:57 +0100

    first commit

---
+++ app/Models/Project.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+
+class Project extends Model
+{
+    use HasFactory;
+    protected $fillable = ['profession_id','title', 'content', 'slug', 'cover_image'];
+
+    public static function generateSlug($title) {
+        return Str::slug($title, '-');
+}
+public function profession() {
+    return $this->belongsTo(Profession::class);
+
+}
+public function technologies() {
+    return $this->belongsToMany(Technology::class);
+}
+}
